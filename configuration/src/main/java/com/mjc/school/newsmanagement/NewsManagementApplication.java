75a659f0f5b10f877d2cba02d13a6d6c4d982fab
sadriddin-suyarov <suyarov-sadriddin@mail.ru>
2023-03-25 15:10:25 +0500

    build and settings files replaced with groovy files; configured build architecture; build project tested;

--- configuration/src/main/java/com/mjc/school/newsmanagement/NewsManagementApplication.java
+++ configuration/src/main/java/com/mjc/school/newsmanagement/NewsManagementApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NewsManagementApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NewsManagementApplication.class, args);
     }
-
 }
--- configuration/src/test/java/com/mjc/school/newsmanagement/NewsManagementApplicationTest.java
+++ configuration/src/test/java/com/mjc/school/newsmanagement/NewsManagementApplicationTest.java
@@ -1,13 +1,16 @@
 package com.mjc.school.newsmanagement;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
-class NewsManagementApplicationTests {
+public class NewsManagementApplicationTest {
 
     @Test
-    void contextLoads() {
+    public void test() {
+        log.info("Everything is working fine");
     }
 
 }
