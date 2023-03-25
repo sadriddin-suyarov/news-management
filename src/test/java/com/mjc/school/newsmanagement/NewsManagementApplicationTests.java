bbd2f062fcc9e96aacad1eb0a7b7cf0730b7a08e
sadriddin-suyarov <suyarov-sadriddin@mail.ru>
2023-03-25 10:16:29 +0500

    initial commit; created new project with previous needed dependencies;

---
+++ src/test/java/com/mjc/school/newsmanagement/NewsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjc.school.newsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
